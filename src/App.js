558e657613aae3886de32f84351d521a99872501
Christian <christian@nyu.edu>
2019-04-24 07:38:13 +0800

    changes to final class version

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Headline from './Headline.js';
 import InputComp from './InputComp.js';
@@ -6,56 +6,46 @@ import superAgent from 'superagent';
 
 class App extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            gifs: [],
-            loading: false
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      gifs: [],
     }
+  }
 
-    //Functions go here
-    findGifs = (term) => {
-        const url = `//api.giphy.com/v1/stickers/search?q=${term}&api_key=SWq0akBClfv0noc1kvCvN8bCBGAKqAZG&limit=10`;
+  //Functions go here
+  findGifs = (term) => {
+    const url = `//api.giphy.com/v1/stickers/search?q=${term}&api_key=SWq0akBClfv0noc1kvCvN8bCBGAKqAZG&limit=10`;
 
-        //use superagent
-        superAgent.get(url).then(res => {
-          console.log(res.body.data[0]);
-        });
-    }
+    //use superagent
+    superAgent.get(url).then(res => {
+      console.log(res.body.data[0]);
+      this.setState({ gifs: res.body.data })
+    });
+  }
 
-    render() {
-        // instead of using ```this.state.gifs``` everywhere, we create a variable.
-        const gifs = this.state.gifs;
+  render() {
+    // instead of using ```this.state.gifs``` everywhere, we create a variable.
+    return (
+      <div className="App">
 
-        return (
-            <div className="App">
+        <Headline greeting={"WECHAT STICKER FINDER!"}></Headline>
+        <InputComp submit={this.findGifs}></InputComp>
+        <div className="display">
 
-                <Headline greeting={"WECHAT STICKER FINDER!"}></Headline>
+          {this.state.gifs.map((gif) => {
+            return (
+              <div className="box">
+                <img src={gif.images.downsized.url} width="200" height="200" />
+              </div>
+            );
+          })
+          }
+        </div>
 
-                <InputComp submit={this.findGifs}></InputComp>
-
-                <div className="display">
-
-                    {this.state.loading
-                        ? <div>Loading...</div>
-                        : gifs && gifs.length > 0
-                            ? gifs.map((gif, index) => {
-                                // ```key``` is required, or react will complain.
-                                return (
-                                    <div className="box" key={index}>
-                                        <img src={gif.images.downsized.url} width="200" height="200"/>
-                                    </div>
-                                );
-
-                            })
-                            : <div>There are no gifs.</div>
-                    }
-                </div>
-
-            </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 export default App;
