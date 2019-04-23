8dc1c0cc0bc3b46c54a040bf94c4eb770952adb9
xu.li <athenalightenedmypath@gmail.com>
2019-04-23 14:55:32 +0800

    add 'loading'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Headline from './Headline.js';
 import InputComp from './InputComp.js';
@@ -6,38 +6,70 @@ import superAgent from 'superagent';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      gifs: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            gifs: [],
+            loading: false
+        }
     }
-  }
 
-  //Functions go here
+    //Functions go here
 
-  findGifs = (term) => {
-    const url = `http://api.giphy.com/v1/stickers/search?q=${term}&api_key=SWq0akBClfv0noc1kvCvN8bCBGAKqAZG&limit=10`;
+    findGifs = (term) => {
+        const url = `//api.giphy.com/v1/stickers/search?q=${term}&api_key=SWq0akBClfv0noc1kvCvN8bCBGAKqAZG&limit=10`;
 
-    superAgent.get(url, (error, response) => {
-      console.log(response.body.data[0]);
-      this.setState({ gifs: response.body.data });
-    });
-  }
+        this.setState({
+            loading: true
+        });
 
-  render() {
-    return (
-      <div className="App">
+        // add 3 seconds of latency so that we can see the loading text.
+        setTimeout(() => {
 
-        <Headline greeting={"WECHAT STICKER FINDER!"}></Headline>
+            // main fetch
+            superAgent.get(url, (error, response) => {
+                console.log(response.body.data[0]);
 
-        <InputComp submit={this.findGifs}></InputComp>
-        <div className = "display">
-          {this.state.gifs.map( gif => <div className="box"><img src={gif.images.downsized.url} width="200" height="200" /></div> )}
-        </div>
-        
-      </div>
-    );
-  }
+                this.setState({
+                    loading: false,
+                    gifs: response.body.data
+                });
+            });
+        }, 3000);
+    }
+
+    render() {
+        // instead of using ```this.state.gifs``` everywhere, we create a variable.
+        const gifs = this.state.gifs;
+
+        return (
+            <div className="App">
+
+                <Headline greeting={"WECHAT STICKER FINDER!"}></Headline>
+
+                <InputComp submit={this.findGifs}></InputComp>
+
+                <div className="display">
+
+                    {this.state.loading
+                        ? <div>Loading...</div>
+                        : gifs && gifs.length > 0
+                            ? gifs.map((gif, index) => {
+                                // ```key``` is required, or react will complain.
+                                return (
+                                    <div className="box" key={index}>
+                                        <img src={gif.images.downsized.url} width="200" height="200"/>
+                                    </div>
+                                );
+
+                            })
+                            : <div>There is no gifs.</div>
+                    }
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
